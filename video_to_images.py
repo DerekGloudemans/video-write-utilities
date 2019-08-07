21d3bce9a9cddc74b5d329a2ed8e969d8192f0bd
Derek <derekgloudemans@gmail.com>
2019-08-07 13:18:32 -0500

    slight modifications to video_to_images.py

--- video_to_images.py
+++ video_to_images.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 from os import mkdir
+from PIL import Image 
+import numpy as np
 
 """
 makes a directory with each frame of the input video file as a separate image. 
@@ -29,37 +31,40 @@ def video_to_images(video_file,out_directory = "temp"):
     
     while ret:  
 
-        cv2.imwrite(out_directory + "/{}.png".format(frame_num),frame)
-        print("On frame: {}, FPS: {:5.2f}".format(frame_num, 1.0 / (time.time() - start)))
+        result = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+        result.save(out_directory + "/{}.bmp".format(frame_num))
+        if frame_num % 50 == 0:
+            print("On frame: {}, FPS: {:5.2f}".format(frame_num, 1.0 / (time.time() - start)))
         frame_num += 1
         start = time.time()
-        # get second frame
+        # get next frame
         ret, frame = cap.read()
-        if frame_num > 30*120:
+        if frame_num > 30*120: # early video cutoff
             cap.release()
             break
-        
-
-video_file = '/media/worklab/data_HDD/cv_data/video/traffic_assorted/traffic_0.avi'
-out_directory = '/media/worklab/data_HDD/cv_data/video/traffic_assorted/traffic_0_frames'
-video_to_images(video_file,out_directory)
 
 class ImDirectoryReader():
     def __init__(self,path):
         self.path = path
         self.frame = 0
     
-    def get(self):
+    def read(self):
         try:
-            im = cv2.imread(self.path+ "/{}.png".format(self.frame))
+            im = cv2.imread(self.path+ "/{}.bmp".format(self.frame))
             self.frame += 1
             return True, im
         except:
             return False, None
         
-    def get_frame(self,frame_num):
+    def read_frame(self,frame_num):
         try:
-            return True, cv2.imread(self.path+ "/{}.png".format(frame_num))
+            return True, cv2.imread(self.path+ "/{}.bmp".format(frame_num))
         except:
             return False, None
-        
+  
+    def isOpened(self):
+        return True
+
+video_file = '/media/worklab/data_HDD/cv_data/video/traffic_assorted/traffic_0.avi'
+out_directory = '/media/worklab/data_HDD/cv_data/video/traffic_assorted/traffic_0_frames'
+video_to_images(video_file,out_directory)      
